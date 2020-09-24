8e234ebd4883bfae95d563c3e762b81e154a6c8c
ERStapleton <68613605+ERStapleton@users.noreply.github.com>
2020-09-24 16:07:22 -0400

    Update scoreboard.sql

--- models/scoreboard.sql
+++ models/scoreboard.sql
@@ -1,8 +1,3 @@
 DROP DATABASE IF EXISTS game_score;  
 
 CREATE DATABASE game_score;
-
-CREATE TABLE game_score (
-    game_id int(10) PRIMARY KEY AUTO_INCREMENT,
-    player_time NOT_NULL DEFAULT CURRENT_TIMESTAMP, 
-);
